cc58aa5701f8e27ee0198948dc6976539832f31c
sapriya <sapriya@visa.com>
2016-07-12 11:32:05 -0700

    Day 5: Trivial Program (.JAR)
    Packaging an application

---
+++ TrivialProgram/src/trivialprogram/TrivialProgram.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trivialprogram;
+
+/**
+ *
+ * @author sapriya
+ */
+public class TrivialProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int a=0;a<args.length;a++){
+        System.out.println(args[a]); 
+        }
+    }
+    
+}
