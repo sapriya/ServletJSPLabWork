68d33e6b185db55ce4459250c4fcd97232755ca3
sapriya <sapriya@visa.com>
2016-07-12 22:56:28 -0700

    Day 5: Exceptional Handling (after reviewing feedback)

--- HandlingExceptions/src/handlingexceptions/HandlingExceptions.java
+++ HandlingExceptions/src/handlingexceptions/HandlingExceptions.java
@@ -4,20 +4,12 @@
  * and open the template in the editor.
  */
 package handlingexceptions;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  * @author SAPRIYA
@@ -38,17 +30,18 @@ public class HandlingExceptions {
         while(true){
             try {
                 inStream = new FileInputStream(inputFile);
-                outStream = new FileOutputStream(outputFile);
-                break;
+                
             } catch (IOException e){
-                if(e.getMessage().contains(inputFile.replace("/","\\"))){
                     System.out.println("Enter valid inputFile name");
                     inputFile = sc.next();
-                }
-                if(e.getMessage().contains(outputFile.replace("/","\\"))){ //Ignored the case where user enters // or \\ 
-                    System.out.println("Enter valid outputFile name");
-                    outputFile = sc.next();
-                }
+                    continue;
+            }
+            try{
+            outStream = new FileOutputStream(outputFile);
+                break;
+            }catch(IOException e){
+                System.out.println("Enter valid outputFile name");
+                outputFile = sc.next();
             }
        }//End while
         
